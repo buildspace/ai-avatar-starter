7d9ecfc92a7cf1b345c836b92660f1a0e9e54308
Alec Dilanchian <alec.dilanchian@gmail.com>
2022-12-16 15:45:11 -0500

    Adding mvp

---
+++ pages/api/generate.js
@@ -0,0 +1,35 @@
+const bufferToBase64 = (buffer) => {
+  const base64 = buffer.toString('base64');
+  return `data:image/png;base64,${base64}`;
+};
+
+const generateAction = async (req, res) => {
+  const input = JSON.parse(req.body).input;
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/adilanchian/dreambooth-ajd-guitar-v1`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
+
+  if (response.ok) {
+    const buffer = await response.buffer();
+    const base64 = bufferToBase64(buffer);
+    res.status(200).json({ image: base64 });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+export default generateAction;
