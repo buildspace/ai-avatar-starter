2c0120b68fdb2e5e2ad4c4bb2cd35a722b862776
maweiche <maweiche@gmail.com>
2023-01-07 12:08:46 -0800

    updated styling and added hard coded prompts

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { render } from 'react-dom';
 
 const Home = () => {
+  const [hardInput1, setHardInput1] = useState('');
+  const [hardInput2, setHardInput2] = useState('');
+  const [hardInput3, setHardInput3] = useState('');
+  const [hardInput4, setHardInput4] = useState('');
   const [input, setInput] = useState('');
   const [img, setImg] = useState('');
   const onChange = (event) => {
@@ -20,6 +25,46 @@ const Home = () => {
 
   // New stateful variable to store image count
   const [imageCount, setImageCount] = useState(0);
+
+//render button container for hardInputs
+  const renderHardInputButtons = () => {
+    return (
+      <div className="input-button-ctnr">
+        <button
+          className="input-button"
+          onClick={() => {
+            setInput(hardInput1);
+          }}
+        >
+          Rembrandt Portrait
+        </button>
+        <button
+          className="input-button"
+          onClick={() => {
+            setInput(hardInput2);
+          }}
+        >
+          Marvel Super Hero
+        </button>
+        <button
+          className="input-button"
+          onClick={() => {
+            setInput(hardInput3);
+          }}
+        >
+          Super Hero
+        </button>
+        <button
+          className="input-button"
+          onClick={() => {
+            setInput(hardInput4);
+          }}
+        >
+          Super Villian
+        </button>
+      </div>
+    );
+  };
 
   // Add generateAction
 const generateAction = async () => {
@@ -43,9 +88,7 @@ const generateAction = async () => {
     setRetry(0);
   }
 
-  // // Replace "raza" or "Raza" in input with "abraza"
-  const input = finalPrompt.replace(/matt/gi, 'matthewweichel');
-
+  
   const response = await fetch('/api/generate', {
     method: 'POST',
     headers: {
@@ -107,6 +150,16 @@ const sleep = (ms) => {
     runRetry();
   }, [retry]);
 
+  //hard input useEffect, hard code pre scripted prompts for user to generate an image
+  useEffect(() => {
+    //rembrandt
+    setHardInput1('matthewweichel portrait in the style of Rembrandt, paying attention to lighting and shadow to create a sense of depth and dimension, intricate, elegant, hgihly detailed, digital painting, smooth, sharp focus, concept art, highly detailed, uhd, 8k, artstation');
+    //marvel/pixar super hero
+    setHardInput2('matthewweichel as Marvel super hero in the style of Pixar animation, 3d render, futuristic art, highly detailed, uhd, 8k, artstation');
+    setHardInput3('highly detailed digital portrait of matthewweichel as a marvel super hero, photo realism, uhd, vibrant colors, artstation, 8k');
+    setHardInput4('highly detailed digital portrait of matthewweichel as a marvel super villian, evil grin, photo realism, uhd, dark colors, shadow depth, artstation, 8k');
+  }, []);
+
   return (
     <div className="root">
       <Head>
@@ -119,9 +172,15 @@ const sleep = (ms) => {
           </div>
           <div className="header-subtitle">
             <h2>
-              Turn me into anyone you want! Make sure you refer to me as "matt" in the prompt
+              Generate a unique avatar of me using AI. <br />
+              Refer to me as 
+                <span>"matthewweichel"</span> 
+              in your prompt.
             </h2>
           </div>
+
+            {renderHardInputButtons()}
+
           {/* Add prompt container here */}
           <div className="prompt-container">
             <input className="prompt-box" value={input} onChange={onChange} />
@@ -154,15 +213,9 @@ const sleep = (ms) => {
         )}
       </div>
       <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <a href="https://www.buymeacoffee.com/beccaandmatt" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: "60px, !important", width: "217px !important"}} /></a>
-          </div>
-        </a>
+        
+            <a href="https://www.buymeacoffee.com/beccaandmatt" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: "20px, !important", width: "180px !important"}} /></a>
+
       </div>
     </div>
   );
