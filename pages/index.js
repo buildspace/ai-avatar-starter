ac24632acb8237cb5fb7003080b862623297a920
ashesashes <ashi.mbamail@gmail.com>
2023-01-12 01:13:27 -0800

    ui commits

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,44 @@
+import { useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Image from 'next/image'; 
+
+//import buildspaceLogo from '../assets/logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  
   return (
-    <div className="root">
-      <Head>
-        <title>AI Avatar Generator | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>your generator one-liner</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>description of your generator</h2>
-          </div>
-        </div>
+<div className="root">
+  <Head>
+    <title> Jungkook Magic Mirror Portraits</title>
+  </Head>
+  <div className = "container">
+    <div className="header">
+      <div className="header-title">
+        <h1>Jungkook Magic Mirror Portraits</h1>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+      <div className="header-subtitle">
+        <h2>
+          Re-imagine Jungkook as a pirate, cyberpunk, or anything!
+        </h2>
       </div>
+      <div className="prompt-container">
+      <input className="prompt-box" value={input} onChange={onChange} />
+        <div className="prompt-buttons">
+          <a className="generate-button">
+      <div className="generate">
+        <p>Generate</p>
+       </div>
+          </a>
+      </div>
+</div>
     </div>
+  </div>
+</div>
   );
 };
 
-export default Home;
+export default Home; 
